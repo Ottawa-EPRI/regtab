c7f65bb54f68769a5795386d444aee43f21b1c5f
Dejan Pavlic <dpavlic@irpe-epri.ca>
2017-10-13 16:52:44 -0400

    Scale down the extent of problem, 1 model at a time.

--- R/regtab.R
+++ R/regtab.R
@@ -19,9 +19,8 @@ retrieve_factor_labels <- function(reg) {
   bind_rows(possible_names)
 }
 
-regtab_single <- function(
+regtab <- function(
   reg,
-  prefix,
   digits = 3,
   pvals = c(`*` = 0.05, `**` = 0.01, `***` = 0.001),
   sumstat_vars = c('adj.r.squared', 'N')
@@ -46,8 +45,7 @@ regtab_single <- function(
       std.error = formatC(std.error, digits = digits, format = 'f'),
       type = 'coef/se'
     ) %>%
-    select('term', 'estimate', 'std.error', 'type') %>%
-    rename_all(~ ifelse(!(.x %in% c('term', 'type')), paste0(.x, prefix), .x))
+    select('term', 'estimate', 'std.error', 'type')
 
   sumstats <- rownames_to_column(as.data.frame(sumstats)) %>%
     mutate(
@@ -56,8 +54,7 @@ regtab_single <- function(
     ) %>%
     mutate_all(as.character) %>%
     filter(rowname %in% sumstat_vars) %>%
-    rename(term = rowname, estimate = V1) %>%
-    rename_all(~ ifelse(!(.x %in% c('term', 'type')), paste0(.x, prefix), .x))
+    rename(term = rowname, estimate = V1)
   table <- bind_rows(tidy_reg, sumstats)
 
   if (length(reg$xlevels) != 0) {
@@ -82,60 +79,4 @@ regtab_single <- function(
     select(-term)
 }
 
-regtab <- function(
-  reg_list,
-  digits = 3,
-  pvals = c(`*` = 0.05, `**` = 0.01, `***` = 0.001),
-  sumstat_vars = c('adj.r.squared', 'N')
-) {
-  init_list <- Map(
-    function(i) regtab_single(reg_list[[i]], i, digits, pvals, sumstat_vars),
-    seq_along(reg_list)
-  )
-  if (length(init_list) > 1) {
-    table <-
-      Reduce(
-        function(x, y) full_join(x, y, by = c('label', 'levels', 'level_order', 'type')),
-        init_list
-      )
-  } else {
-    table <- init_list[[1]]
-  }
-
-  order_split <- split(table, forcats::fct_inorder(factor(table$label)))
-  table <- bind_rows(Map(
-    function(x) {
-      if (all(!is.na(x$level_order))) {
-        arrange(x, level_order)
-      } else {
-        x
-      }
-    },
-    order_split
-  ))
-
-  table %<>%
-    select(label, levels, level_order, type, everything())
-
-  # What a HACK. Separate out sumstat and non-sumstat types, only to splat
-  # them back on top.
-  table_nss <- filter(table, type != 'sumstat')
-  table_ss <- filter(table, type == 'sumstat')
-  bind_rows(table_nss, table_ss)
-}
-
-bottom_se <- function(reg, parens = TRUE) {
-  if (parens) {
-    reg$model$std.error <- paste0('(', reg$std.error, ')')
-  }
-  est_se_interleave <- c(rbind(reg$estimate, reg$std.error))
-  term_interleave <- c(rbind(reg$term, rep('', length(reg$term))))
-  reg$model <- data.frame(term = term_interleave, estimate = est_se_interleave)
-  reg
-}
-
-z <- lm(Sepal.Length ~ factor(Sepal.Width), iris)
-u <- lm(Sepal.Length ~ Sepal.Width, iris)
-g <- regtab(list(z))
-#bottom_se(g)
 
