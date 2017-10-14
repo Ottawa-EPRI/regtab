fefca234b946ada40522c87aee7bd176a69c8f9b
Dejan Pavlic <dpavlic@irpe-epri.ca>
2017-10-14 12:18:04 -0400

    Return properly sorted regression table.
    This still does not deal in any way with
    interacted variables or multiple specs, and
    how it functions outside of built-in models
    is still very much an open question. But, it
    does provide a better start.

--- R/regtab.R
+++ R/regtab.R
@@ -31,21 +31,25 @@ regtab <- function(
 
   sumstats <- t(cbind(broom::glance(reg), N = nobs(reg)))
   tidy_reg <- broom::tidy(reg)
-  sorted_p <- sort(pvals, decreasing = TRUE)
-  for (i in seq_along(sorted_p)) {
-    tidy_reg %<>% mutate(
-      est.sig = if_else(p.value <= sorted_p[i], names(sorted_p)[i], '')
-    )
+  if (!is.null(pvals)) {
+    sorted_p <- sort(pvals, decreasing = TRUE)
+    for (i in seq_along(sorted_p)) {
+      tidy_reg %<>% mutate(
+        est.sig = if_else(p.value <= sorted_p[i], names(sorted_p)[i], '')
+      )
+    }
   }
 
   tidy_reg %<>%
     mutate(
-      estimate = paste0(formatC(estimate, digits = digits, format = 'f'),
-                        est.sig),
+      estimate = formatC(estimate, digits = digits, format = 'f'),
       std.error = formatC(std.error, digits = digits, format = 'f'),
       type = 'coef/se'
-    ) %>%
-    select('term', 'estimate', 'std.error', 'type')
+    )
+  if ('est.sig' %in% names(tidy_reg)) {
+    tidy_reg %<>%
+      mutate(estimate = ifelse(is.na(est.sig) | est.sig == '', estimate, paste0(estimate, est.sig)))
+  }
 
   sumstats <- rownames_to_column(as.data.frame(sumstats)) %>%
     mutate(
@@ -57,17 +61,9 @@ regtab <- function(
     rename(term = rowname, estimate = V1)
   table <- bind_rows(tidy_reg, sumstats)
 
-  if (length(reg$xlevels) != 0) {
-    table %<>%
-      full_join(levels, by = 'term')
-  } else {
-    table %<>%
-      mutate(
-        label = NA_character_,
-        levels = NA_character_,
-        level_order = NA_integer_
-      )
-  }
+  # FIXME: What if there are NO LEVELS
+  table %<>%
+    full_join(levels, by = 'term')
 
   table %<>%
     mutate(
@@ -75,8 +71,14 @@ regtab <- function(
       type = ifelse(is.na(type) & level_order == 1, 'omitted', type)
     )
 
-  table %>%
-    select(-term)
+  labels <- unique(table$label)
+  table %<>%
+    left_join(tibble(label = labels, label_ix = seq_along(labels))) %>%
+    arrange(label_ix, level_order)
+
+  table %<>%
+    select(-matches('term|statistic|p.value|label_ix|est.sig')) %>%
+    select(label, levels, level_order, everything())
+
+  table
 }
-
-
