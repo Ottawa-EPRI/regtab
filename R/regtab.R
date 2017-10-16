4747b11a1ce232874c6fba84d672b7f77ab2dbcb
Dejan Pavlic <dpavlic@irpe-epri.ca>
2017-10-16 12:15:04 -0400

    Change factor label retrieval to label retrieval.
    Still to do are interactions.

--- R/regtab.R
+++ R/regtab.R
@@ -1,22 +1,39 @@
-retrieve_factor_labels <- function(reg) {
-  # FIXME: What happens when there are no levels
-  #reg_labels <- attributes(reg$terms)$term.labels
+retrieve_labels <- function(reg, tidy_reg) {
   reg_factor_levels <- reg$xlevels
 
-  possible_names <- Map(
-    function(x, y) {
-      data.frame(
-        term = paste0(x, y),
-        label = x,
-        levels = y
-      ) %>%
-        mutate(level_order = 1:n()) %>%
-        mutate_if(is.factor, as.character)
-    },
-    names(reg_factor_levels),
-    reg_factor_levels
-  )
-  bind_rows(possible_names)
+  # This has clear problems outside base models, like mlogit that uses ':' for
+  # different outcomes. We'll need to think about how to deal with that. Maybe
+  # with just a class check, I doubt we want S3 methods all over the place, but
+  # maybe.
+  terms_no_interactions <- tidy_reg %>%
+    filter(!grepl(':', term)) %>%
+    select(term)
+
+  if (length(reg_factor_levels) != 0) {
+    possible_names <- Map(
+      function(x, y) {
+        data.frame(
+          term = paste0(x, y),
+          label = x,
+          levels = y
+        ) %>%
+          mutate(level_order = 1:n()) %>%
+          mutate_if(is.factor, as.character)
+      },
+      names(reg_factor_levels),
+      reg_factor_levels
+    )
+    terms_no_interactions <- bind_rows(possible_names) %>%
+      full_join(terms_no_interactions, by = 'term')
+  } else {
+    terms_no_interactions %<>%
+      mutate(
+        label = NA_character_, levels = NA_character_, level_order = NA_integer_
+      )
+  }
+
+  terms_no_interactions %>%
+      mutate(label = ifelse(is.na(label), term, label))
 }
 
 regtab <- function(
@@ -25,12 +42,11 @@ regtab <- function(
   pvals = c(`*` = 0.05, `**` = 0.01, `***` = 0.001),
   sumstat_vars = c('adj.r.squared', 'N')
 ) {
-  if (length(reg$xlevels) != 0) {
-    level_df <- retrieve_factor_labels(reg)
-  }
 
   sumstats <- t(cbind(broom::glance(reg), N = nobs(reg)))
   tidy_reg <- broom::tidy(reg)
+
+  level_df <- retrieve_labels(reg, tidy_reg)
   if (!is.null(pvals)) {
     sorted_p <- sort(pvals, decreasing = TRUE)
     for (i in seq_along(sorted_p)) {
@@ -63,18 +79,17 @@ regtab <- function(
     mutate_all(as.character) %>%
     filter(rowname %in% sumstat_vars) %>%
     rename(term = rowname, estimate = V1)
-  table <- bind_rows(tidy_reg, sumstats)
+  table <- bind_rows(tidy_reg, sumstats) %>%
+    full_join(level_df, by = 'term')
 
   # FIXME: What if there are NO LEVELS
-  if (exists("level_df")) {
-    table %<>%
-      full_join(level_df, by = 'term')
-  } else {
-    table %<>%
-      mutate(
-        label = NA_character_, levels = NA_character_, level_order = NA_integer_
-      )
-  }
+  # FIXME X 2
+  #if (exists("level_df")) {
+  #  table %<>%
+  #    full_join(level_df, by = 'term')
+  #} else {
+  #  table %<>%
+  #}
 
   table %<>%
     mutate(
@@ -94,3 +109,6 @@ regtab <- function(
 
   table
 }
+
+z <- lm(Sepal.Length ~ factor(Sepal.Width), data = iris)
+z <- lm(Sepal.Length ~ Sepal.Width, data = iris)
